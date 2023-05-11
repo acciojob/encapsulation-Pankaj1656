9747d4a55a9ebba131e26c356ed5d2f4ecf8144c
Pankaj1656 <pc69269@gmail.com>
2023-05-11 15:21:14 +0530

    first

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,8 +2,10 @@ package com.driver;
 
 public class Main {
     public static void main (String[]args){
-        System.out.print("Hello");
+
         RWOnly obj=new RWOnly();
+        obj.setName("Pankaj");
+        System.out.println(obj.getName());
     }
 
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private  String name;
+     public void  setName(String N){
+         this.name=N;
+     }
+     public String getName(){
+         return name;
+     }
 }
